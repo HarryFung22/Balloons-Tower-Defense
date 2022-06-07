fc8b99076389293b7a3cde59dbadd4fef5499fd5
HarryFung22 <106692548+HarryFung22@users.noreply.github.com>
2022-06-07 12:11:39 -0400

    Add files via upload

---
+++ Group3BTD7/Group3BTD7/Balloons.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.List;
+/**
+ * Write a description of class Balloons here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public abstract class Balloons extends Actor
+{
+    int health, speed;
+    boolean camo = false;
+    boolean metal = false;
+    boolean removeMe = false;
+    public void act()
+    {
+        // Add your action code here.
+    }
+    
+    
+}
