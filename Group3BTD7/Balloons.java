60229be0dfb01216eb83e5ed20412682c6db8e7a
HarryFung22 <106692548+HarryFung22@users.noreply.github.com>
2022-06-11 22:27:33 -0400

    Revert "updated game world"
    This reverts commit a5efa70dafc115f779053d8a745a0bdc99f4d3a3.

--- Group3BTD7/Balloons.java
+++ Group3BTD7/Balloons.java
@@ -23,47 +23,5 @@ public abstract class Balloons extends Actor
         }
     }
     
-    public void delete(){
-        if(removeMe == true){
-            getWorld().removeObject(this);
-            ((GameWorld) getWorld()).setHealth(health);
-        }
-    }
     
-    public void onPath(){
-        move(speed);
-        List<Pathing> path90 = getObjectsAtOffset(-25,-25,  Pathing.class);
-        for(Pathing paths: path90)
-        {
-            if(paths.straight == false && getRotation() == 0)
-            {
-                setRotation(paths.rotation);
-            }
-        }
-        List<Pathing> path0 = getObjectsAtOffset(-25,-25, Pathing.class);
-        for(Pathing paths: path0)
-        {
-            if(paths.straight == false && getRotation() == 90)
-            {
-                setRotation(paths.rotation);
-            }
-        }
-        List<Pathing> path270 = getObjectsAtOffset(-25,-25, Pathing.class);
-        for(Pathing paths: path270)
-        {
-            if(paths.straight == false && getRotation() == 180)
-            {
-                setRotation(paths.rotation);
-            }
-        }
-        List<Pathing> path180 = getObjectsAtOffset(20,20, Pathing.class);
-        for(Pathing paths: path180)
-        {
-            if(paths.straight == false && getRotation() == 270)
-            {
-                setRotation(paths.rotation);
-            }
-        }
-        removeMe = true;
-    }
 }
